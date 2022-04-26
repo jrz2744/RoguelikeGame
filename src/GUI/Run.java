ca23d78d6214391d2cfe5631e08b96044b767df2
Frank Andes <sfandes@gmail.com>
2022-04-26 17:26:32 -0400

    properties reader and writer added to the run and weapons files, debug line included but commented out

--- src/GUI/Run.java
+++ src/GUI/Run.java
@@ -1,8 +1,17 @@
 package GUI;
 
+import java.io.*;
+import java.util.Properties;
+
+import static java.lang.System.out;
+
 public class Run {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Properties p = new Properties();
+        p.setProperty("1", "0-50");
+        p.store(new FileWriter("myFile.properties"), "Weapon Properties");
 
     }
 }
--- src/Player/Weapon.java
+++ src/Player/Weapon.java
@@ -1,7 +1,16 @@
 package Player;
 
+import Common.Coordinates;
 import Common.Effect;
 import Common.Item;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Properties;
+
+import static java.lang.System.getenv;
+import static java.lang.System.out;
 
 public class Weapon implements Item {
 
@@ -10,7 +19,26 @@ public class Weapon implements Item {
     private int[] dmgRange;
 
     public Weapon(int id){
+        // the id of the desired weapon
         this.id = id;
+
+        // attempt to read the weapon properties file
+        Properties p = new Properties();
+        try {
+            p.load(new FileReader("myFile.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Debug line to see what the properties file is holding
+        //p.list(new PrintStream(out, true));
+
+        // read properties file and set damage range depending on the id of the weapon
+        String[] tempRange = p.getProperty(String.valueOf(id)).split("-");
+        this.dmgRange = new int[tempRange.length];
+        for (int i = 0; i < tempRange.length; i++) {
+            this.dmgRange[i] = Integer.parseInt(tempRange[i]);
+        }
     }
 
     public void createMap(){
@@ -27,8 +55,8 @@ public class Weapon implements Item {
     }
 
     @Override
-    public int[] getCoordinate() {
-        return new int[0];
+    public Coordinates getCoordinate() {
+        return null;
     }
 
     @Override
